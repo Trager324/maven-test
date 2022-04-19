4510ba0d34d308197fa2b06b48b8bd1a0e4e3ec1
syd <583129837@qq.com>
2022-04-20 00:33:55 +0800

    feat: graduate

--- java17/src/test/java/com/syd/java17/AppTest.java
+++ java17/src/test/java/com/syd/java17/AppTest.java
@@ -3,6 +3,9 @@ package com.syd.java17;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
 
 /**
  * @author SYD
