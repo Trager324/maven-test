1672358e60aa257072efa1a191e3c22e2b93f3d9
songyide <58312837@qq.com>
2022-06-14 18:22:34 +0800

    feat: 改进设计类方法

--- java17/src/main/java/com/syd/java17/struct/Solution.java
+++ java17/src/main/java/com/syd/java17/struct/Solution.java
@@ -1,21 +1,20 @@
 package com.syd.java17.struct;
 
-import com.alibaba.fastjson.JSON;
-import com.alibaba.fastjson.JSONArray;
-import com.alibaba.fastjson.JSONAware;
-import com.alibaba.fastjson.JSONObject;
-import com.syd.java17.util.algo.MathUtils;
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-import lombok.ToString;
+import com.alibaba.fastjson.*;
+import lombok.*;
+import lombok.experimental.Accessors;
+import lombok.extern.java.Log;
 
+import java.io.*;
 import java.lang.reflect.*;
 import java.math.BigDecimal;
 import java.math.BigInteger;
+import java.math.MathContext;
 import java.math.RoundingMode;
+import java.time.*;
 import java.util.*;
 import java.util.concurrent.*;
+import java.util.function.*;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 import java.util.stream.*;
@@ -28,52 +27,40 @@ import static com.syd.java17.struct.TreeNode.parseTreeNode;
  */
 @NoArgsConstructor
 public class Solution {
+    static final int[][] DIRS = {{-1, 1}, {1, -1}};
 
-    public List<String> findAndReplacePattern(String[] words, String pattern) {
-        char[] set1 = new char[128], set2 = new char[128], cp = pattern.toCharArray();
-        int n = cp.length;
-        List<String> res = new ArrayList<>();
-        for (String word : words) {
-            char[] cw = word.toCharArray();
-            int i = 0;
-            for (; i < n; i++) {
-                if (set1[cp[i]] == 0) {
-                    if (set2[cw[i]] == 0) {
-                        set1[cp[i]] = cw[i];
-                        set2[cw[i]] = cp[i];
-                    } else {
-                        break;
-                    }
-                } else if (set1[cp[i]] != cw[i]) {
-                    break;
+    public int[] findDiagonalOrder(int[][] mat) {
+        int i = 0, j = 0, m = mat.length, n = mat[0].length, len = m * n, cnt = 0, dir = 0;
+        int[] res = new int[len];
+        while (true) {
+            res[cnt++] = mat[i][j];
+            if (cnt == len) {
+                break;
+            }
+            i += DIRS[dir][0];
+            j += DIRS[dir][1];
+            if (i < 0 || i >= m || j < 0 || j >= n) {
+                if (dir == 0) {
+                    i++;
+                } else {
+                    j++;
                 }
-            }
-            if (i == n) {
-                res.add(word);
-            }
-            for (int j = 'a'; j <= 'z'; j++) {
-                set1[j] = set2[j] = 0;
+                dir = 1 ^ dir;
+                while (i < 0 || i >= m || j < 0 || j >= n) {
+                    i += DIRS[dir][0];
+                    j += DIRS[dir][1];
+                }
             }
         }
         return res;
     }
 
     public static void main(String[] args) throws Exception {
-        System.out.println(SOLUTION.findAndReplacePattern(parseStringArray("[\"abc\",\"deq\",\"mee\",\"aqq\",\"dkd\",\"ccc\"]"), "abb"));
-        System.out.println(SOLUTION.findAndReplacePattern(parseStringArray("[\"a\",\"b\",\"c\"]"), "a"));
+        System.out.println(Arrays.toString(SOLUTION.findDiagonalOrder(parseIntMatrix("[[1,2,3],[4,5,6],[7,8,9]]"))));
+        System.out.println(Arrays.toString(SOLUTION.findDiagonalOrder(parseIntMatrix("[[1,2,3,4,5],[6,7,8,9,10],[11,12,13,14,15],[16,17,18,19,20]]"))));
     }
 
     static final Solution SOLUTION = new Solution();
-
-    public static void printStringList(Collection<?> list) {
-        System.out.print("[");
-        Iterator<?> it = list.iterator();
-        int n = list.size() - 1;
-        for (int i = 0; i < n; i++) {
-            System.out.printf("\"%s\", ", it.next());
-        }
-        System.out.printf("\"%s\"]", it.next());
-    }
 
     public static <T> T parseObject(String text, Class<T> clazz) {
         return JSON.parseObject(text, clazz);
@@ -104,18 +91,21 @@ public class Solution {
     }
 
     public static String array2Str(Object[] array) {
-        StringBuilder sb = new StringBuilder("[");
-        for (Object o : array) {
-            String tmp = o instanceof String ? "\"" + o + "\"" : o.toString();
-            sb.append(tmp).append(",");
-        }
-        if (sb.length() > 1) {
-            sb.deleteCharAt(sb.length() - 1);
-        }
-        return sb.append("]").toString();
+        return list2Str(Arrays.asList(array));
     }
 
-    public static Executable[] getExecutableArray(Class<?> clazz, String[] methodNames, String[][] argLists)
+    static boolean isMatchedExecutable(Executable e, Object[] args) {
+        Class<?>[] types = e.getParameterTypes();
+        if (types.length != args.length) return false;
+        for (int j = 0; j < args.length; j++) {
+            if (types[j] != args[j].getClass()) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static Executable[] getExecutableArray(Class<?> clazz, String[] methodNames, Object[][] argsArray)
             throws NoSuchMethodException {
         int n = methodNames.length;
         Method[] methods = clazz.getDeclaredMethods();
@@ -124,27 +114,24 @@ public class Solution {
         Executable[] res = new Executable[n];
         for (int i = 0; i < n; i++) {
             String methodName = methodNames[i];
-            String[] argList = argLists[i];
+            Object[] argArray = argsArray[i];
             if (clazzName.equals(methodName)) {
                 for (Constructor<?> ctor : ctors) {
-                    if (ctor.getParameterCount() == argList.length) {
+                    if (isMatchedExecutable(ctor, argArray)) {
                         res[i] = ctor;
                         break;
                     }
                 }
-                if (res[i] == null) {
-                    throw new NoSuchMethodException(clazzName + "(" + array2Str(argList) + ")");
-                }
             } else {
                 for (Method method : methods) {
-                    if (method.getName().equals(methodName) && method.getParameterCount() == argList.length) {
+                    if (method.getName().equals(methodName) && isMatchedExecutable(method, argArray)) {
                         res[i] = method;
                         break;
                     }
                 }
-                if (res[i] == null) {
-                    throw new NoSuchMethodException(clazzName + "." + methodName + "(" + array2Str(argList) + ")");
-                }
+            }
+            if (res[i] == null) {
+                throw new NoSuchMethodException(clazzName + "." + methodName + "(" + array2Str(argArray) + ")");
             }
             res[i].setAccessible(true);
         }
@@ -154,13 +141,12 @@ public class Solution {
     public static Object[] invokeResults(Class<?> clazz, String execStr, String argsStr)
             throws NoSuchMethodException, InvocationTargetException, InstantiationException, IllegalAccessException {
         String[] methodNames = parseStringArray(execStr);
-        String[][] argLists = parseObject(argsStr, String[][].class);
-        if (methodNames.length != argLists.length) {
-            throw new IllegalArgumentException("methodNames.length != argLists.length");
+        Object[][] argsArray = parseObject(argsStr, Object[][].class);
+        if (methodNames.length != argsArray.length) {
+            throw new IllegalArgumentException("methodNames.length != argsArray.length");
         }
 
-        Executable[] executables = getExecutableArray(clazz, methodNames, argLists);
-        Object[][] argsArray = parseObject(argsStr, Object[][].class);
+        Executable[] executables = getExecutableArray(clazz, methodNames, argsArray);
         int n = executables.length;
         Object[] res = new Object[n];
         Object obj = null;
@@ -178,13 +164,17 @@ public class Solution {
     @NoArgsConstructor
     @AllArgsConstructor
     @ToString
+    @EqualsAndHashCode
+    @Accessors(chain = true)
+    @Log
     static class DTO {
         String id;
         AbstractMap<BigInteger, BigDecimal> concurrentMap = new ConcurrentHashMap<>();
-        Map<Date, Arrays> map = new TreeMap<>();
+        Map<Instant, Clock> map = new TreeMap<>();
         List<Collections> list = new CopyOnWriteArrayList<>();
         Queue<Calendar> collection = new ConcurrentLinkedDeque<>();
-        JSONAware json = JSONObject.parseArray(new JSONArray().toJSONString());
+        JSONAware jsonAware = JSONPath.compile("$.id");
+        JSON json = JSONObject.parseArray(new JSONArray().toJSONString());
         Field field = (Field)Proxy.newProxyInstance(DTO.class.getClassLoader(), DTO.class.getInterfaces(),
                 (o, m, a) -> m.invoke(o, a));
         ExecutorService service = Executors.newCachedThreadPool();
@@ -192,9 +182,14 @@ public class Solution {
         Matcher matcher = pattern.matcher("");
         RoundingMode mode = RoundingMode.CEILING;
         IntStream intStream = StreamSupport.intStream(Spliterators.emptyIntSpliterator(), true);
-        DoubleStream doubleStream = StreamSupport.doubleStream(Spliterators.emptyDoubleSpliterator(), true);
-        Stream<Integer> stream = Arrays.stream(new int[0]).boxed().collect(Collectors.toList()).stream();
-
+        BaseStream<Long, LongStream> longStream = LongStream.of().unordered();
+        Stream<InputStream> stream = Arrays.stream(new BufferedInputStream[0]);
+        Function<LocalDateTime, LocalTime> function = LocalDateTime::toLocalTime;
+        Supplier<LocalDate> supplier = LocalDate::now;
+        Consumer<Duration> consumer = __ -> {
+        };
+        BiFunction<FileReader, FileWriter, File> biFunction = (r, w) -> new File("");
+        Predicate<MathContext> intSupplier = __ -> true;
 
         public static void main(String[] args) {
             parseTreeNode("[]");
